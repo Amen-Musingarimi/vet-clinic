0fa7905d9cc2451c552c041b1170fcf4dfa52c2e
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-05-02 22:56:40 +0200

    add more queries

--- queries.sql
+++ queries.sql
@@ -1,8 +1,65 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 BEGIN;
+
 UPDATE animals
 SET species = 'unspecified';
 SELECT * FROM animals;
+
 ROLLBACK;
+
 SELECT * FROM animals;
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT my_savepoint;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO my_savepoint;
+
+UPDATE animals
+SET weight = weight * -1
+WHERE weight < 0;
+
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT name, neutered, escape_attempts
+FROM animals
+WHERE escape_attempts = (
+    SELECT MAX(escape_attempts)
+    FROM animals
+)
+
+SELECT species, AVG(escape_attempts) as avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
