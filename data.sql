97aa7d0c1026379ab69463a6f1e33572445e0ff1
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-05-04 12:33:14 +0200

    Insert data in vets table

--- data.sql
+++ data.sql
@@ -1,38 +1,9 @@
 /* Populate database with sample data. */
--- Inserting data in the owners table
-INSERT INTO owners (full_name, age)
-VALUES 
-('Sam Smith', 34),
-('Jennifer Orwell', 19),
-('Bob', 45),
-('Melody Pond', 77),
-('Dean Winchester', 14),
-('Jodie Whittaker', 38);
-
--- Inserting data in the owners table
-INSERT INTO species (name)
-VALUES 
-('Pokemon'),
-('Digimon');
-
--- Inserting species_id in animals table
-UPDATE animals 
-SET species_id = 
-  CASE 
-    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
-    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
-  END;
-
--- Inserting owners_id in animals table
-UPDATE animals 
-SET owner_id = 
-  CASE 
-    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
-    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
-    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
-    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
-    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
-  END;
-
-
+-- Inserting data in the vets table
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES
+  ('William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08');
 
